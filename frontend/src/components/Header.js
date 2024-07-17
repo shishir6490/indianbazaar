18f7e8bb5bd95acbe1c11e2c43dafa5824cf1247
shishir6490 <53550985+shishir6490@users.noreply.github.com>
2024-07-17 23:10:54 +0530

    Create Header.js

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,12 @@
+// frontend/src/components/Header.js
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      <h1>IndianBazaar</h1>
+    </header>
+  );
+}
+
+export default Header;
