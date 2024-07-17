986d76986b3eb8b7c91d0beec3f735141df4d4da
shishir6490 <53550985+shishir6490@users.noreply.github.com>
2024-07-17 23:43:25 +0530

    Update App.test.js

--- frontend/src/__tests__/App.test.js
+++ frontend/src/__tests__/App.test.js
@@ -1,23 +1,22 @@
 // frontend/src/__tests__/App.test.js
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, waitFor } from '@testing-library/react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from '../pages/App';
 
-test('renders Dashboard heading', () => {
+test('renders Dashboard heading', async () => {
   const { getByText, debug } = render(
     <Router>
       <App />
     </Router>
   );
-  
+
   // Debugging the rendered output
   debug();
 
-  // More flexible matching function
-  const headingElement = getByText((content, element) => {
-    return element.tagName.toLowerCase() === 'h1' && content.match(/dashboard/i);
+  // Wait for the Dashboard component to be rendered
+  await waitFor(() => {
+    const headingElement = getByText(/dashboard/i);
+    expect(headingElement).toBeInTheDocument();
   });
-
-  expect(headingElement).toBeInTheDocument();
 });
