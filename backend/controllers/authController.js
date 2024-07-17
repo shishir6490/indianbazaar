036a09a68b8cfe6844eea83f4c848d8c6a9313ba
shishir6490 <53550985+shishir6490@users.noreply.github.com>
2024-07-17 13:19:01 +0530

    Create authController.js

---
+++ backend/controllers/authController.js
@@ -0,0 +1,11 @@
+const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+const config = require('../config/config');
+
+exports.register = async (req, res) => {
+    // Registration logic
+};
+
+exports.login = async (req, res) => {
+    // Login logic
+};
