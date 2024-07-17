45b8b78f9d2c8af0a97e13d87f0d7219c634f681
shishir6490 <53550985+shishir6490@users.noreply.github.com>
2024-07-17 13:19:31 +0530

    Create inventoryController.js

---
+++ backend/controllers/inventoryController.js
@@ -0,0 +1,9 @@
+const Inventory = require('../models/Inventory');
+
+exports.addItem = async (req, res) => {
+    // Add item logic
+};
+
+exports.getInventory = async (req, res) => {
+    // Get inventory logic
+};
