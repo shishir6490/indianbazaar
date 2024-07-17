9cf7364f429a0c5461bb9dc91953d7f0added25d
shishir6490 <53550985+shishir6490@users.noreply.github.com>
2024-07-17 23:09:25 +0530

    Update Dashboard.js

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -1,11 +1,12 @@
+// frontend/src/pages/Dashboard.js
 import React from 'react';
 
-const Dashboard = () => {
-    return (
-        <div>
-            <h1>Dashboard</h1>
-        </div>
-    );
-};
+function Dashboard() {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+    </div>
+  );
+}
 
 export default Dashboard;
