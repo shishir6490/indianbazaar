f62f95bf91db85ed902a232b7989011ba4a97b38
shishir6490 <53550985+shishir6490@users.noreply.github.com>
2024-07-17 23:27:48 +0530

    Update App.test.js

--- frontend/src/__tests__/App.test.js
+++ frontend/src/__tests__/App.test.js
@@ -1,13 +1,13 @@
 // frontend/src/__tests__/App.test.js
 import React from 'react';
 import { render } from '@testing-library/react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import App from '../pages/App';
 
 test('renders Dashboard heading', () => {
   const { getByText } = render(
     <Router>
-      <App />
+      <Route path="/dashboard" component={App} />
     </Router>
   );
   const headingElement = getByText(/dashboard/i);
