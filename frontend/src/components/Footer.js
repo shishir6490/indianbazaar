e95344fe1b340314d035ded7996900205abf2a10
shishir6490 <53550985+shishir6490@users.noreply.github.com>
2024-07-17 23:11:13 +0530

    Create Footer.js

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,12 @@
+// frontend/src/components/Footer.js
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <p>&copy; 2024 IndianBazaar</p>
+    </footer>
+  );
+}
+
+export default Footer;
