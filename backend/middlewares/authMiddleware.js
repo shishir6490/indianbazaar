ee375a52842b257d7a32069007eea3d6e6de69bc
shishir6490 <53550985+shishir6490@users.noreply.github.com>
2024-07-17 13:19:57 +0530

    Create authMiddleware.js

---
+++ backend/middlewares/authMiddleware.js
@@ -0,0 +1,8 @@
+const jwt = require('jsonwebtoken');
+const config = require('../config/config');
+
+const authMiddleware = (req, res, next) => {
+    // Authentication logic
+};
+
+module.exports = authMiddleware;
