49da00e42cd6fdafb250c1ce4c9e9f4779943bf8
shishir6490 <53550985+shishir6490@users.noreply.github.com>
2024-07-17 23:02:33 +0530

    Update App.test.js

--- frontend/src/__tests__/App.test.js
+++ frontend/src/__tests__/App.test.js
@@ -1,6 +1,7 @@
+// frontend/src/__tests__/App.test.js
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from '../App';
+import App from '../pages/App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
