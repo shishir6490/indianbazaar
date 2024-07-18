ceeee79adab5582130cd7f2ba2ee6e219e3dc147
shishir6490 <53550985+shishir6490@users.noreply.github.com>
2024-07-18 20:08:31 +0530

    Update App.test.js

--- frontend/src/__tests__/App.test.js
+++ frontend/src/__tests__/App.test.js
@@ -1,8 +1,8 @@
 // src/__tests__/App.test.js
 import React from 'react';
 import { render, screen, waitFor } from '@testing-library/react';
-import '@testing-library/jest-dom'; // This ensures jest-dom is used
-import App from '../App';
+import '@testing-library/jest-dom/extend-expect'; // This ensures jest-dom is used
+import App from '../pages/App'; // Update this path based on your project structure
 
 test('renders Dashboard heading', async () => {
   render(<App />);
